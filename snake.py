aef901fb6fccc574a49906a32581309906e54345
galaxyNGC224 <lisnyk@ukr.net>
2022-02-06 22:46:59 +0200

    snake added 2

--- snake.py
+++ snake.py
@@ -16,14 +16,14 @@ BLUE = (0, 0, 255)
 
 pygame.init()
 screen = pygame.display.set_mode((SIZE, SIZE))
-x, y = random.randrange(0, FIELD), random.randrange(0, FIELD)
+x, y = FIELD // 2, FIELD // 2
 snake = [[x, y]]
 
 length = 1
 dx, dy = 1, 0
-apple = [random.randrange(0, FIELD), random.randrange(0, FIELD)]
+apple = [random.randrange(1, FIELD - 1), random.randrange(1, FIELD - 1)]
 while snake[0] == apple:
-    apple = [random.randrange(0, FIELD), random.randrange(0, FIELD)]
+    apple = [random.randrange(1, FIELD - 1), random.randrange(1, FIELD - 1)]
 
 pygame.display.set_caption('Hallo')
 clock = pygame.time.Clock()
@@ -36,15 +36,34 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            pass
-
+            if (event.key == pygame.K_UP) and (dy != 1):
+                dx, dy = 0, -1
+            if (event.key == pygame.K_DOWN) and (dy != -1):
+                dx, dy = 0, 1
+            if (event.key == pygame.K_LEFT) and (dx != 1):
+                dx, dy = -1, 0
+            if (event.key == pygame.K_RIGHT) and (dx != -1):
+                dx, dy = 1, 0
     x += dx
     y += dy
-    snake.append([x, y])
-    snake = snake[length:]
 
+    for i, j in snake:
+        if (x == i) and (y == j):
+            running = False
+
+    if running and (x < 0 or x > FIELD or y < 0 or y > FIELD):
+        running = False
+
+    if apple == [x, y]:
+        length += 1
+        apple = [random.randrange(1, FIELD - 1), random.randrange(1, FIELD - 1)]
+        #FPS += 1
+    snake.append([x, y])
+    snake = snake[-length:]
+
+    [pygame.draw.rect(screen, GREEN, (i * TILE, j * TILE, TILE - BORDER, TILE - BORDER)) for i in range(FIELD) for j in range(FIELD)]
     [pygame.draw.rect(screen, RED, (i * TILE, j * TILE, TILE - BORDER, TILE - BORDER)) for i, j in snake]
-    pygame.draw.rect(screen, GREEN, (apple[0] * TILE, apple[1] * TILE, TILE - BORDER, TILE - BORDER))
+    pygame.draw.rect(screen, BLUE, (apple[0] * TILE, apple[1] * TILE, TILE - BORDER, TILE - BORDER))
 
     pygame.display.flip()
 
